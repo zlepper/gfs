0c5aa271b75c12fa4f63e967dc6894938a3eae49
Zlepper <hansen13579@gmail.com>
2017-07-03 06:37:31 +0200

    Work on client
    Progres on #1

--- client.go
+++ client.go
@@ -1,16 +1,18 @@
 package gfs
 
 import (
-	"net/http"
-	"path"
 	"bytes"
 	"encoding/json"
 	"errors"
+	"io"
+	"net/http"
+	"os"
+	"path"
 )
 
 type Client struct {
-	url string
-	token string
+	url    string
+	token  string
 	client *http.Client
 }
 
@@ -18,10 +20,10 @@ func (c *Client) getUrl(p string) string {
 	return path.Join(c.url, p)
 }
 
-func (c *Client) Login(username, password string) (error) {
+func (c *Client) Login(username, password string) error {
 	loginRequest := LoginRequest{
-		Username:username,
-		Password:password,
+		Username: username,
+		Password: password,
 	}
 
 	var buf bytes.Buffer
@@ -41,7 +43,6 @@ func (c *Client) Login(username, password string) (error) {
 		return err
 	}
 	defer resp.Body.Close()
-
 
 	var response AuthorizationResponse
 	err = json.NewDecoder(resp.Body).Decode(&response)
@@ -68,7 +69,7 @@ func (c *Client) getContent(p string, out interface{}) error {
 		return err
 	}
 
-	req.Header.Set("token", c.token)
+	req.Header.Set("gfs-token", c.token)
 	req.Header.Set("accept", FormatJson)
 
 	resp, err := c.client.Do(req)
@@ -99,9 +100,61 @@ func (c *Client) GetFileData(p string) (*FileStats, error) {
 	return &stats, err
 }
 
+// Requirements for uploading a file to GFS
+type UploadFile struct {
+	// The name of the file to upload
+	Filename string
+	// The file reader that allows reading the file
+	Reader io.ReadCloser
+	// The length of the file
+	FileLength int64
+}
+
+// Creates a new instance of upload file.
+func NewUploadFile(filename string, reader io.ReadCloser, fileLength int64) UploadFile {
+	return UploadFile{
+		Filename:   filename,
+		Reader:     reader,
+		FileLength: fileLength,
+	}
+}
+
+// Helper method to quickly create new UploadFile from a file on the disk
+// Make sure to call `f.Reader.Close()` to make sure no leaks happens in
+// case of errors
+func NewUploadFileFromDisk(filepath string) (f UploadFile, err error) {
+	stats, err := os.Stat(filepath)
+	if err != nil {
+		return f, err
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return f, err
+	}
+
+	return NewUploadFile(path.Base(filepath), file, stats.Size()), nil
+}
+
+func (c *Client) UploadFiles(files []UploadFile, targetPath string) error {
+	//
+	//rp, wp, err := os.Pipe()
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//bodyWriter := multipart.NewWriter(wp)
+	//
+	//
+	//
+	//bodyWriter.CreateFormFile()
+
+	return errors.New("gfs.UploadFiles is not yet supported.")
+}
+
 func NewClient(url, username, password string) (*Client, error) {
 	client := Client{
-		url:url,
+		url: url,
 	}
 
 	err := client.Login(username, password)
